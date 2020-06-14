a163891911dd354ef2faf92c8eab6b06f4d42b6f
przemekkot <65823229+przemekkot@users.noreply.github.com>
2020-06-14 15:56:09 +0200

    Fix convert from str to int and float
    In this commit following parts of code have been fixed:
    - converting input values from str to int where necessary and float
    - distinction for name operation taking into consideration the operation input value
    - programming exit option before number1 and number2 inputs

--- calculator.py
+++ calculator.py
@@ -4,25 +4,32 @@ logging.basicConfig(level=logging.DEBUG)
 
 def calculator(operation, number1, number2):
   result = ''
-  if operation == 1:
-    result = number1 + number2
-  elif operation == 2:
-    result = number1 - number2
-  elif operation == 3:
-    result = number1 * number2
-  elif operation == 4:
-    result = number1 / number2
-  else:
-    print("Niepoprawny kod działania")
-    exit(1)
+  if int(operation) == 1:
+    result = float(number1) + float(number2)
+  elif int(operation) == 2:
+    result = float(number1) - float(number2)
+  elif int(operation) == 3:
+    result = float(number1) * float(number2)
+  elif int(operation) == 4:
+    result = float(number1) / float(number2)
   return result
 
 
 if __name__ == "__main__":
   operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+  if int(operation) not in [1, 2, 3, 4]:
+    logging.debug("Niepoprawny kod działania")
+    exit(1)
   number1 = input("Podaj pierwszą wartość liczbową działania: ")
   number2 = input("Podaj drugą wartość liczbową działania: ")
-  logging.debug("Dodaję %s i %s" % (number1, number2))
+  if int(operation) == 1:
+    logging.debug("Dodaję %s i %s" % (number1, number2))
+  elif int(operation) == 2:
+    logging.debug("Odejmuję %s i %s" % (number1, number2))
+  elif int(operation) == 3:
+    logging.debug("Mnożę %s i %s" % (number1, number2))
+  elif int(operation) == 4:
+    logging.debug("Dzielę %s i %s" % (number1, number2))
   result = calculator(operation, number1, number2)
-  logging.debug("Wynik to %s" % result)
-  calculator(operation, number1, number2)
+  logging.debug("Wynik to %.2f" % result)
+calculator(operation, number1, number2)
